1a345b41ede0e7a14eaad88b8204ba92d1f8d8a3
zenghongcong <zenghongcong@baogongyoucai.com>
2019-05-08 12:10:30 +0800

    标题自定义配置

--- src/views/customer/home/index.js
+++ src/views/customer/home/index.js
@@ -1,7 +1,10 @@
 import Vue from "vue";
 import App from "./index.vue";
-import "@/common"
+import store from "@/store";
+import "@/common";
+import "@/utils";
 
 new Vue({
+  store,
   render: h => h(App)
 }).$mount("#app");
